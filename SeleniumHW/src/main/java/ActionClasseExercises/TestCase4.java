8bebb974c0da45821f22580ef6162334c6a51495
MomoJW <mojw2014@gmail.com>
2022-08-29 17:05:42 -0400

    setup

--- SeleniumHW/src/main/java/ActionClasseExercises/TestCase4.java
+++ SeleniumHW/src/main/java/ActionClasseExercises/TestCase4.java
@@ -1,5 +1,19 @@
 package ActionClasseExercises;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class TestCase4 {
 
+	public static void main(String[] args) {
+		
+		String key = "webdriver.chrome.driver";
+		String path = "C:\\Users\\mojw2\\OneDrive\\Desktop\\B7-Selenium\\chromedriver.exe";
+		System.setProperty(key, path);
+		WebDriver driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+	}
 }
